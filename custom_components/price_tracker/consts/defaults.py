4f16b6401c7f1501d39ecb59ccf1db89995b7053
Hwajin Lee <brightdelusion@gmail.com>
2025-03-18 00:26:27 +0900

    :bug: Fix random method

--- custom_components/price_tracker/consts/defaults.py
+++ custom_components/price_tracker/consts/defaults.py
@@ -1,6 +1,6 @@
 DOMAIN = "price_tracker"
 NAME = "E-Commerce Price Tracker"
 DESCRIPTION = "Track the price of products on e-commerce websites"
-VERSION = "1.4.7"
+VERSION = "1.4.8"
 PLATFORMS = ["sensor"]
 DATA_UPDATED = f"{DOMAIN}_data_updated"
