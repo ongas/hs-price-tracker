2ce060a15ef851b3827e045478308bf020e93383
Hwajin Lee <brightdelusion@gmail.com>
2025-03-18 00:22:34 +0900

    :bug: Fix SMARTSTORE

--- custom_components/price_tracker/consts/defaults.py
+++ custom_components/price_tracker/consts/defaults.py
@@ -1,6 +1,6 @@
 DOMAIN = "price_tracker"
 NAME = "E-Commerce Price Tracker"
 DESCRIPTION = "Track the price of products on e-commerce websites"
-VERSION = "1.4.5"
+VERSION = "1.4.7"
 PLATFORMS = ["sensor"]
 DATA_UPDATED = f"{DOMAIN}_data_updated"
